75cf344b92dc6dcdb75d5b368884584a18a9442d
Vitória Nauanda <vnauanda@gmail.com>
2022-08-02 21:27:00 -0300

    Conhecendo o JSX

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -2,6 +2,7 @@ const FirstComponent = () => {
     return (
         <div>
             <h1>Meu primeiro componente!</h1>
+            <p className = "Teste"> Meu texto</p>
         </div>
 
     );
