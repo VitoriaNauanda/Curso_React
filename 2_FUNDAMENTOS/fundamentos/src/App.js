6718b27fe87ed6358f3eca2e6746832c8b8072a4
Vitória Nauanda <vnauanda@gmail.com>
2022-08-02 21:12:54 -0300

    Importando componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,13 @@
-
+//Components//
+import FirstComponent from './components/FirstComponent';
+//Styles - CSS//
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent/>
     </div>
   );
 }
