5407c5365b14959dcd87706c4020c649c4cc7a21
Vitória Nauanda <vnauanda@gmail.com>
2022-08-05 19:25:58 -0300

    Tarefa 3: Desafio sobre criação de apps em React

---
+++ AvancarReact/secao3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 fontsize = '5'> Tarefa 3: Desafio sobre criação de apps em React</h1>
+    </div>
+  );
+}
+
+export default App;
