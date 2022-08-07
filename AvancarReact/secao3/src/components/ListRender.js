c2a15d92fde38f57ded4fafd563b6c06240d0bf7
Vitória Nauanda <vnauanda@gmail.com>
2022-08-07 19:24:12 -0300

    A propriedade key

--- AvancarReact/secao3/src/components/ListRender.js
+++ AvancarReact/secao3/src/components/ListRender.js
@@ -2,11 +2,21 @@ import { useState } from "react"
 
 const ListRender = () => {
     const [list] = useState(["Mateus", "Pedro", "Josias", "Maria"]);
+    const [users] = useState([
+        {id: 1, name: "Mateus", age: 31},
+        {id: 2785646, name: "João", age: 28},
+        {id: 84521, name: "Pedro", age: 44},
+    ])
     return(
         <div>
             <ul> 
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
         </div>
