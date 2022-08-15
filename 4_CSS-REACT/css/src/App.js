5d1caca3ff2cd494cee84b5f116cd8473c0cae41
Vitória Nauanda <vnauanda@gmail.com>
2022-08-15 20:21:45 -0300

    Css inline dinamico

--- 4_CSS-REACT/css/src/App.js
+++ 4_CSS-REACT/css/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
-
+import { useState } from "react"
 function App() {
+  const n = 15
+  const [name] = useState("Matheus")
   return (
     <div className="App">
       {/*CSS global */}
@@ -12,6 +14,10 @@ function App() {
       {/*Inline css */}
       <p style={{color:"magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
       <p style={{color:"magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+      {/*Inline dinamico */}
+      <h2 style={n < 10 ? ({color:"purple"}) : ({color:"pink"})}>CSS dinamico</h2>
+      <h2 style={n > 10 ? ({color:"purple"}) : ({color:"pink"})}>CSS dinamico</h2>
+      <h2 style={name === "Matheus" ? ({color:"green", backgroundColor: "#000"}) : null}>Teste nome</h2>
     </div>
   );
 }
